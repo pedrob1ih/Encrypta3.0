30dd0639675c27a33d89222e1ff321b483bbd3ba
Dropee <pedrob1ih@gmail.com>
2015-12-13 13:24:08 +0100

    primerCommit

---
+++ src/encrypta3/Encrypta3.java
@@ -0,0 +1,16 @@
+package encrypta3;
+
+import encrypta3.Clases.Encryptacion;
+
+
+public class Encrypta3 {
+
+    
+    public static void main(String[] args) {
+        Encryptacion e= new Encryptacion("Fich");
+        long asda=1123234234;
+        System.out.println(e.factorial(asda));
+        System.out.println(asda);
+        
+    }    
+}
